51651f2ba75d430afc1f252c02bd4a9ecb3511fc
Thiyagu-k <esandhaithiyagu@gmail.com>
2024-09-12 15:01:43 +0530

    vite-config.js adjustment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,10 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue()
+        vue(),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',  // Ensure the manifest is built in the correct location
+    },
 });
