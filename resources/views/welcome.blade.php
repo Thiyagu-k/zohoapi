7191feb2801ceb6d5508978fe281f7765c97a3b7
Thiyagu-k <esandhaithiyagu@gmail.com>
2024-09-13 12:23:15 +0530

    vite-config.js adjustment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,13 @@
     <!-- DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     
-    
-    @vite('resources/css/app.css')
+    <!-- styles -->
+    @vite(['resources/js/app.js','resources/css/app.css'])
 </head>
 <body>
     <div id="app">
-        <zoho-sales-orders></zoho-/sales-orders>
+        <zoho-sales-orders></zoho-/sal00es-orders>
     </div>
-    @vite('resources/js/app.js')
     <!-- DataTables JS -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
